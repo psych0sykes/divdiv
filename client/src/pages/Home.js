b06fe952c5d98f03eecf2509e5b08236fa15c5c6
blake <blake@tests-mbp.uguest.utah.edu>
2020-01-16 21:15:20 -0700

    Added pages, and a Home page component. Added API to src and getDivs route

---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
+
+class Home extends Component {
+
+}
---
+++ client/src/utils/API.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export default {
+  getDivs: function() {
+    return axios.get("/api/divs");
+  },
+
+//   getBook: function(id) {
+//     return axios.get("/api/books/" + id);
+//   },
+//   // Deletes the book with the given id
+//   deleteBook: function(id) {
+//     return axios.delete("/api/books/" + id);
+//   },
+//   // Saves a book to the database
+//   saveBook: function(bookData) {
+//     return axios.post("/api/books", bookData);
+//   }
+// };
