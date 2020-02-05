9ca258c5e19064538f27dc228f569972446db1d1
psych0sykes <blakesykes13@gmail.com>
2020-02-04 18:18:47 -0700

    Completed the basic layout of the canvasView page

--- client/src/components/Canvas/CanvasBio/index.js
+++ client/src/components/Canvas/CanvasBio/index.js
@@ -9,6 +9,11 @@ function CanvasBio(props) {
             <p>
                 {props.bio}
             </p>   
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus justo dolor, tincidunt efficitur odio hendrerit quis. Pellentesque sed viverra felis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce facilisis lacinia pretium. Donec scelerisque lobortis tellus vel gravida. Duis laoreet at libero iaculis scelerisque.
+                 Morbi eleifend posuere turpis, sed posuere sapien tincidunt in. 
+                In nisl justo, tristique in purus at, volutpat vehicula dui. Cras a leo ut libero rutrum sodales.
+            </p>
         </div>
     )
 }
--- client/src/pages/CanvasView.js
+++ client/src/pages/CanvasView.js
@@ -14,6 +14,8 @@ class CanvasCreate extends Component {
         username: "",
         canvas_title: "",
         bio: "",
+        support_total: "",
+        donation_total: ""
     }
 
     componentDidMount() {
@@ -35,10 +37,28 @@ class CanvasCreate extends Component {
             console.log(res.data)
             this.setState({
                 canvas_title: res.data.canvas_title,
-                bio: res.data.bio
+                bio: res.data.bio,
+                support_total: res.data.support_total,
+                donation_total: this.formatMoney(res.data.donation_total,0,".",",")
                 })
         }).catch((err) => console.log(err));
     }
+
+    formatMoney = (amount, decimalCount, decimal, thousands) => {
+        try {
+          decimalCount = Math.abs(decimalCount);
+          decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+      
+          const negativeSign = amount < 0 ? "-" : "";
+      
+          let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+          let j = (i.length > 3) ? i.length % 3 : 0;
+      
+          return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+        } catch (e) {
+          console.log(e)
+        }
+      };
 
     render() {
         return(
@@ -54,12 +74,24 @@ class CanvasCreate extends Component {
                             <CanvasBio bio={this.state.bio}/>
                         </div>
                         <div className="col-md-6">
-                            <Row>
-                                <button className="createDivButton">
-                                    add a div
-                                </button>
-                                <p>show your support</p>
-                            </Row>
+                            <div className="scorecardWrapper">
+                                <div className="canvasScorecard">
+                                    <h1>{this.state.support_total}</h1>
+                                    <p>supporters</p>
+                                </div>
+                                <div className="canvasScorecard">
+                                    <h1>${this.state.donation_total}</h1>
+                                    <p>funds raised</p>
+                                </div>
+                            </div>
+                            <FlexRow>
+                                <div className="bioButtonLabel">
+                                    <button className="createDivButton">
+                                        add a div
+                                    </button>
+                                </div>
+                                <p>help support this canvas</p>
+                            </FlexRow>
                         </div>
                     </FlexRow>
                     <Canvas canvasId={this.props.match.params.id} divSize="75px" border="none"/>
