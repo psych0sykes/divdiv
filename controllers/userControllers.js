8e45bfb8e49a7b106935bd94e20d4c743ce2f74c
psych0sykes <blakesykes13@gmail.com>
2020-09-07 22:32:35 -0600

    Cleaned up user api controller and removed the .env from git

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -12,12 +12,29 @@ module.exports = {
         db.User
             .findOne({username: req.params.username})
             .then(user => {
-                return res.json(user)
+                const resUser = {
+                    username: user.username,
+                    email: user.email,
+                    first_name: user.first_name,
+                    last_name: user.last_name,
+                    created: user.created,
+                };
+                
+                return res.json(resUser)
             })
             .catch(err => res.status(422).json(err));
     }
 }
 
+// const userSchema = new Schema({
+//     username: {type: String, required: true},
+//     password: {type: String, required: true},
+//     email: {type: String, required: true},
+//     first_name: {type: String},
+//     last_name: {type: String},
+//     created: { type: Date, default: Date.now }
+  
+//   });
 
 
 
